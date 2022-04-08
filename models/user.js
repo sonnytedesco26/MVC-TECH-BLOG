e55f5e23427fa38b9328c681939957560a01bb70
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-04-08 10:10:34 -0400

    wrote up public js for everything

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// create our User model
+// create user model
 class User extends Model {
   // checks password
   checkPassword(loginPw) {
